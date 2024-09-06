5c9b65e551c3483c85a10a789b0a0a5fd0b6c824
shaykotselim <hossainshaykot@gmail.com>
2024-09-07 03:49:10 +0600

    create nav

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -7,37 +7,40 @@ import { FiAlignJustify } from "react-icons/fi";
 
 const Header = () => {
   return (
-    <div className="navbar bg-gradient-to-t from-blue-800 lg:from-blue-400 to-purple-800 lg:to-blue-800 h-28">
+    <div className="navbar bg-gradient-to-t from-blue-400 lg:from-blue-200 to-purple-300 lg:to-blue-500 h-28">
       <div className="navbar-start">
         <div className="dropdown lg:flex lg:gap-9">
-          <div tabIndex={0} role="button" className="btn bg-gradient-to-t from-blue-800 to-purple-800 text-white lg:hidden">
+          <div tabIndex={0} role="button" className="text-4xl text-white lg:hidden">
             <FiAlignJustify />
           </div>
           <ul
             tabIndex={0}
-            className=" menu-sm dropdown-content mt-3 z-[1] p-2   rounded-box w-40 "
+            className=" menu-sm dropdown-content mt-3 z-[1] p-2   rounded w-40 bg-blue-400"
           >
-             <li className="text-white  border-b-2 px-4 py-3 rounded bg-gradient-to-r from-blue-800 to-blue-400 hover:bg-gradient-to-r hover:from-blue-400 hover:to-blue-800 cursor-pointer">
+             <li className="text-white  border-b-2 px-4 py-3 ">
             <Link href="/">Home</Link>
           </li>
-          <li className="text-white  border-b-2 px-4 py-3 rounded bg-gradient-to-r from-blue-800 to-blue-400 hover:bg-gradient-to-r hover:from-blue-400 hover:to-blue-800 cursor-pointer">
+          <li>
+     
+          </li>
+          <li className="text-white  border-b-2 px-4 py-3 ">
             <Link href="/">Blog</Link>
           </li>
-          <li className="text-white  border-b-2 px-4 py-3 rounded bg-gradient-to-r from-blue-800 to-blue-400 hover:bg-gradient-to-r hover:from-blue-400 hover:to-blue-800 cursor-pointer">
+          <li className="text-white  border-b-2 px-4 py-3">
             <Link href="/">Glarry</Link>
           </li>
-          <li className="text-white border-b-2 px-4 py-3 rounded bg-gradient-to-r from-blue-800 to-blue-400 hover:bg-gradient-to-r hover:from-blue-400 hover:to-blue-800 cursor-pointer">
+          <li className="text-white border-b-2 px-4 py-3 ">
             <Link href="/">Contact us</Link>
           </li>
-          <li className="text-white  border-b-2 px-4 py-3 rounded bg-gradient-to-r from-blue-800 to-blue-400 hover:bg-gradient-to-r hover:from-blue-400 hover:to-blue-800 cursor-pointer">
+          <li className="text-white  border-b-2 px-4 py-3 ">
             <Link href="/">About</Link>
           </li>
           </ul>
         </div>
       </div>
-      <div className="ml-24 lg: lg:navbar-center lg:-ml-[600px]  ">
+      <div className="ml-24 lg: lg:navbar-center lg:-ml-[600px]  rounded-full  ">
         <Image
-          className="rounded h-16 lg:h-24 w-auto"
+          className="rounded h-16 lg:h-28 w-auto"
           alt="Lamkain-Student-Council"
           src={logo}
         />
@@ -45,19 +48,19 @@ const Header = () => {
       {/*---------------- For Desktop Area------------- */}
       <div className="hidden lg:flex lg:ml-[600px]">
         <ul className=" flex gap-4 px-1">
-          <li className="text-white  border-b-2 px-4 py-3 rounded bg-gradient-to-r from-blue-800 to-blue-400 hover:bg-gradient-to-r hover:from-blue-400 hover:to-blue-800 cursor-pointer">
+          <li className="text-white border-b-2 px-4 py-3">
             <Link href="/">Home</Link>
           </li>
-          <li className="text-white  border-b-2 px-4 py-3 rounded bg-gradient-to-r from-blue-800 to-blue-400 hover:bg-gradient-to-r hover:from-blue-400 hover:to-blue-800 cursor-pointer">
+          <li className="text-white  border-b-2 px-4 py-3 ">
             <Link href="/">Blog</Link>
           </li>
-          <li className="text-white  border-b-2 px-4 py-3 rounded bg-gradient-to-r from-blue-800 to-blue-400 hover:bg-gradient-to-r hover:from-blue-400 hover:to-blue-800 cursor-pointer">
+          <li className="text-white  border-b-2 px-4 py-3">
             <Link href="/">Glarry</Link>
           </li>
-          <li className="text-white  border-b-2 px-4 py-3 rounded bg-gradient-to-r from-blue-800 to-blue-400 hover:bg-gradient-to-r hover:from-blue-400 hover:to-blue-800 cursor-pointer">
-            <Link href="/">Contact us</Link>
+          <li className="text-white  border-b-2 px-4 py-3">
+            <Link href="/">Contact</Link>
           </li>
-          <li className="text-white  border-b-2 px-4 py-3 rounded bg-gradient-to-r from-blue-800 to-blue-400 hover:bg-gradient-to-r hover:from-blue-400 hover:to-blue-800 cursor-pointer">
+          <li className="text-white  border-b-2 px-4 py-3">
             <Link href="/">About</Link>
           </li>
         </ul>
